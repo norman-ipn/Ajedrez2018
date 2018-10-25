c30a8f992104218e2822ff6fd9ccd1469a2c086f
YaronDavid <urlof_desty91@hotmail.com>
2018-10-25 16:07:33 -0500

    Funciones alfil, rey y reina

--- src/movimientos.h
+++ src/movimientos.h
@@ -1,9 +1,18 @@
 #ifndef __MOVIMIENTOS_H__
 #define __MOVIMIENTOS_H__
+#include <stdio.h>
 int moverPeon (int* , int* , int , int , int , int , int );
 int comerPeon (int* , int* , int , int , int , int , int );
 int moverInicialmentePeon(int* , int*, int , int , int , int , int );
 int validarMovimientoPeon ( int* , int* , int , int , int );
-
+void moverAlfil (char, int *, int *, int , int );
+void comerAlfil (char, int, int);
+void hacerJaqueAlfil (int, int, int, int);
+void moverRey (char, int *, int *, int);
+void comerRey (char, int, int);
+void detectarJaqueRey ();
+void moverReina (char, int *, int *, int, int);
+void comerReina (char, int, int);
+void hacerJaqueReina (int, int, int, int);
 
 #endif
