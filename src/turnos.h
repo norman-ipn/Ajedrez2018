3ca9ccb5b24161785c00cb65332b09c8f33b8d0e
JohanCampos <hugojohan.camposocampo@gmail.com>
2018-10-25 15:55:28 -0500

    Añadido de centinela en turnos.h

--- src/turnos.h
+++ src/turnos.h
@@ -1,5 +1,8 @@
+#ifndef __TURNOS_H__
+#define __TURNOS_H__
 char cambiarTurno (char);
 int ingresarComando (char, char);
 void indicarTurno (char);
 int moverPiezaIndicada (int, int, int, int);
 void mostrarTiempo (int);
+#endif
