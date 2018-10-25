0317fc3419ffe39b50c8f0574c046e51640f20fd
JakelineSalgado <jdsalgado01@gmail.com>
2018-10-25 15:35:57 -0500

    restricciones

---
+++ src/restricciones.h
@@ -0,0 +1,7 @@
+int verificarDestinoDesocupado(int);
+int verificarMovimientoTorre(int);
+int verificarMovimientoRey(int);
+int verificarMovimientoReina(int);
+int verificarMovimientoAlfil(int);
+int verificarMovimientoCaballo(int);
+int verificarMovimientoPeon(int);
