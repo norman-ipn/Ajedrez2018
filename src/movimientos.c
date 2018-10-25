419d66d5a47c8f54b1b60e593b4da5c4529a7b60
AlejandroEnriquezRivera <alexenriquezr@hotmail.com>
2018-10-25 10:06:21 -0500

    se agrego la prueba del printf

---
+++ src/movimientos.c
@@ -0,0 +1,9 @@
+#include "movimientos.h"
+
+int
+moverPeon (int *filaInicialPeon, int *columnaInicialPeon, int filaDestinoPeon,
+	   int columnaDestinoPeon, int colorPeon, int filaReyEnemigo,
+	   int columnaReyEnemigo)
+{
+  printf ("Prueba mover peon");
+}
--- src/movimientos.h
+++ src/movimientos.h
@@ -1,5 +1,6 @@
 #ifndef __MOVIMIENTOS_H__
 #define __MOVIMIENTOS_H__
+#include <stdio.h>
 int moverPeon (int* , int* , int , int , int , int , int );
 int comerPeon (int* , int* , int , int , int , int , int );
 int moverInicialmentePeon(int* , int*, int , int , int , int , int );
