80b07c74ede82a48c764bcee5c001aa4fd0ef4de
David Madrigal Buendía <madrigal.bd@gmail.com>
2018-11-02 17:05:07 -0600

    Se agregaron las funciones que faltaban de movimientos.h

--- src/movimientos.h
+++ src/movimientos.h
@@ -1,10 +1,17 @@
 #ifndef __MOVIMIENTOS_H__
 #define __MOVIMIENTOS_H__
 #include <stdio.h>
-int moverPeon (int* , int* , int , int , int , int , int );
-int comerPeon (int* , int* , int , int , int , int , int );
-int moverInicialmentePeon(int* , int*, int , int , int , int , int );
-int validarMovimientoPeon ( int* , int* , int , int , int );
+int moverPeon (int *, int *, int, int, int, int, int);
+int comerPeon (int *, int *, int, int, int, int, int); 
+int moverInicialmentePeon (int *, int *, int, int, int, int, int);
+int coronar (int, int *, int, int);
+int hacerJaquePeon (int, int, int, int, int);
+int moverTorre (int *, int *, int, int, int, int, int);
+int comerTorre (int *, int *, int, int, int, int, int);
+int hacerJaqueTorre (int, int, int, int, int);
+int moverCaballo (int *, int *, int, int, int, int, int);
+int comerCaballo (int *, int *, int, int, int, int, int);
+int hacerJaqueCaballo (int, int, int, int, int);
 void moverAlfil (char, int *, int *, int , int );
 void comerAlfil (char, int, int);
 void hacerJaqueAlfil (int, int, int, int);
