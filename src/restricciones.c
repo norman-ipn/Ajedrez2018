55f36bdb354761caf0705436e30335e7a70b24f7
LuisRamírez2000 <l_armando_2000@hotmail.com>
2018-11-07 17:58:33 -0600

    Añadido el código de restricción de movimiento para el rey

--- src/restricciones.c
+++ src/restricciones.c
@@ -1,10 +1,50 @@
 #include "restricciones.h"
 
 
-int verificarMovimientoTorre(int coordenadaPieza, int filaPieza, int coordenadaMovimiento, int filaMovimiento)
+int
+verificarMovimientoTorre (int coordenadaPieza, int filaPieza,
+			  int coordenadaMovimiento, int filaMovimiento)
 {
 
-printf("prueba\n");
+  printf ("prueba\n");
 
 
+}
+
+int
+verificarMovimientoRey (int coordenadaPieza, int filaPieza,
+			int coordenadaMovimiento, int filaMovimiento)
+{
+  int calcularDistanciaFila = 0;
+  int calcularDistanciaColumna = 0;
+
+  if (coordenadaPieza > coordenadaMovimiento)
+    {
+      calcularDistanciaColumna = coordenadaPieza - coordenadaMovimiento;
+    }
+
+  if (coordenadaPieza < coordenadaMovimiento)
+    {
+      calcularDistanciaColumna = coordenadaMovimiento - coordenadaPieza;
+    }
+
+  if (calcularDistanciaColumna != 1)
+    {
+      return 1;
+    }
+
+if (filaPieza > filaMovimiento)
+  {
+    calcularDistanciaFila = filaPieza - filaMovimiento;
+  }
+
+if (filaPieza > filaMovimiento)
+  {
+    calcularDistanciaFila = filaMovimiento - filaPieza;
+  }
+
+if (calcularDistanciaFila != 1)
+  {
+    return 1;
+  }
 }
