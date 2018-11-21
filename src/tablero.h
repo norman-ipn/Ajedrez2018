e475c4e80300d9cd0ef435dc9a1fafab184a482a
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2018-11-20 23:46:25 -0600

    Actualización a tablero.h

--- src/tablero.h
+++ src/tablero.h
@@ -2,24 +2,8 @@
 #define __TABLERO_H__
 
 #include<stdio.h>
-void imprimirTablero (int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int, int, int, int, int, int, int,
-		      int, int, int, int, int);
+
+void imprimirTablero (int tablero[][8]);
 int recibirCordenadas (int, int, int);
 
 #endif
