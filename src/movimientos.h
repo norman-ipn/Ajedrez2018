0a26d53b966dec89d1f76d063fbff97d9778777a
David Madrigal Buendía <madrigal.bd@gmail.com>
2018-11-21 22:28:12 -0600

    Se actualizaron las cabeceras de las funciones de Peon, Torre y Caballo

--- src/movimientos.h
+++ src/movimientos.h
@@ -5,17 +5,17 @@
 #include "tablero.h"
 #include "restricciones.h"
 
-int moverPeon (int *, int *, int, int, char, int, int);
-int comerPeon (int *, int *, int, int, char, int, int);
-int moverInicialmentePeon (int *, int *, int, int, char);
-int coronar (int, int *, int, int);
-int hacerJaquePeon (int, int, char, int, int);
-int moverTorre (int *, int *, int, int);
-int comerTorre (int *, int *, int, int, int, int);
-int hacerJaqueTorre (int, int, char, int, int);
-int moverCaballo (int *, int *, int, int, int, int, int);
-int comerCaballo (int *, int *, int, int, int, int, int);
-int hacerJaqueCaballo (int, int, int, int, int);
+int moverPeon (int *, int *);
+int comerPeon (int *, int *);
+int moverInicialmentePeon (int *, int *);
+int coronar (int *, int);
+int hacerJaquePeon (int[8][8]);
+int moverTorre (int *, int *);
+int comerTorre (int *, int *);
+int hacerJaqueTorre (int[8][8]);
+int moverCaballo (int *, int *);
+int comerCaballo (int *, int *);
+int hacerJaqueCaballo (int[8][8]);
 void moverAlfil (int *, int *, int, int);
 void comerAlfil (int *, int *, int *, int *);
 int hacerJaqueAlfil (int, int, int, int, char);
