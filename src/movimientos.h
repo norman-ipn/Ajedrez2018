660b5e481206f3a37912723c46fd206484d51fb6
David Madrigal Buendía <madrigal.bd@gmail.com>
2018-11-22 00:26:10 -0600

    Actualización de las funciones de Peon, Caballo, Torre

--- src/movimientos.h
+++ src/movimientos.h
@@ -5,17 +5,17 @@
 #include "tablero.h"
 #include "restricciones.h"
 
-int moverPeon (int *, int *);
-int comerPeon (int *, int *);
-int moverInicialmentePeon (int *, int *);
-int coronar (int *, int);
-int hacerJaquePeon (int[8][8]);
-int moverTorre (int *, int *);
-int comerTorre (int *, int *);
-int hacerJaqueTorre (int[8][8]);
-int moverCaballo (int *, int *);
-int comerCaballo (int *, int *);
-int hacerJaqueCaballo (int[8][8]);
+void moverPeon (int *, int *);
+void comerPeon (int *, int *);
+void moverInicialmentePeon (int *, int *);
+void coronar (int *, int);
+int hacerJaquePeon (int[8][8], int, int, int, int);
+void moverTorre (int *, int *);
+void comerTorre (int *, int *);
+int hacerJaqueTorre (int[8][8], int, int, int, int);
+void moverCaballo (int *, int *);
+void comerCaballo (int *, int *);
+int hacerJaqueCaballo (int[8][8], int, int, int, int);
 void moverAlfil (int *, int *, int, int);
 void comerAlfil (int *, int *, int *, int *);
 int hacerJaqueAlfil (int, int, int, int, char);
