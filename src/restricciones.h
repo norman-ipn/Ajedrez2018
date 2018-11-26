bf10b08f4def00736daaea3736b4bdda74545f15
Carol258 <Crisya78@gmail.com>
2018-11-26 00:54:39 -0600

    Parametros de verificarMovimientoReina

--- src/restricciones.h
+++ src/restricciones.h
@@ -6,7 +6,7 @@
 int verificarDestinoDesocupado (int, int);
 int verificarMovimientoTorre (int[8][8], int, int, int, int);
 int verificarMovimientoRey (int[8][8], int, int, int, int);
-int verificarMovimientoReina (int, int, int, int);
+int verificarMovimientoReina (int[8][8], int, int, int, int);
 int verificarMovimientoAlfil (int, int, int, int);
 int verificarMovimientoCaballo (int, int, int, int);
 int verificarMovimientoPeon (int[8][8], int, int, int, int);
